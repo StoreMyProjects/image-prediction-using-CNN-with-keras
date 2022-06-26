eb78f92fd2b9ecc0e4a38d86382af85bdf2bf126
amrendra01 <singhamrendra1999@gmail.com>
2022-06-26 10:24:49 +0530

    MyWebPro urls.py updated

--- MyWebPro/urls.py
+++ MyWebPro/urls.py
@@ -19,5 +19,5 @@ from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('imageUpload/', include('ImageRecognition.urls'))
+    path('upload/', include('ImageRecognition.urls'))
 ]
